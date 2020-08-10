2afe76037144645a5194f4728717190dd20345cd
Alucard <smpark@ezstorage.co.kr>
2020-08-10 20:14:09 +0900

    add comments collection

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { ApolloServer, gql } = require("apollo-server");
 const lodashId = require("lodash-id");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
+const format = require("date-fns/format");
 
 const adapter = new FileSync("db.json");
 const db = low(adapter);
@@ -9,6 +10,7 @@ const db = low(adapter);
 db._.mixin(lodashId);
 
 const collection = db.get("products");
+const commentsCollection = db.get("comments");
 
 const typeDefs = gql`
   type Product {
@@ -16,30 +18,46 @@ const typeDefs = gql`
     name: String
     price: String
     imgUrl: String
+    comments: [Comment]
+  }
+
+  type Comment {
+    id: ID
+    productId: ID
+    content: String
+    createdAt: String
+    score: Float
   }
 
   type Query {
     products: [Product]
+    getProduct(id: ID!): Product
   }
 
   type Mutation {
+    # Product
     addProduct(name: String, price: String, imgUrl: String): Product
     deleteProduct(id: ID): String
+
+    # Comment
+    addComment(content: String!, score: Float, productId: ID!): Comment
   }
 `;
 
 const resolvers = {
   Query: {
+    // Product:
     products: () => {
       return db.get("products");
+    },
+    getProduct: (_, { id }) => {
+      const product = db.get("products").find({ id }).value();
+      const comments = db.get("comments").filter({ productId: id }).value();
+      return { ...product, comments };
     },
   },
   Mutation: {
     addProduct: (_, { name, price, imgUrl }) => {
-      console.log({ name, price, imgUrl });
-      // const collection = db.get("products");
-      console.log("collection");
-      console.log(collection);
       const newItem = collection
         .insert({
           name,
@@ -54,6 +72,18 @@ const resolvers = {
       // const collection = db.get('products')
       const result = collection.removeById(id).write();
       return result.id;
+    },
+    addComment: (_, { content, score, productId }) => {
+      const newItem = commentsCollection
+        .insert({
+          content,
+          score,
+          productId,
+          createdAt: format(new Date(), "yyyy/MM/dd"),
+        })
+        .write();
+
+      return newItem;
     },
   },
 };
